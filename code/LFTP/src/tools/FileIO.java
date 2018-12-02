b664a988e3dd063da2196d2fc2042f428d31667c
Carey <944131226@qq.com>
2018-12-02 21:58:41 +0800

    解决超2GB大文本分块读写skip bug

--- code/LFTP/src/tools/FileIO.java
+++ code/LFTP/src/tools/FileIO.java
@@ -5,6 +5,7 @@ package tools;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
@@ -52,50 +53,58 @@ public class FileIO {
 	
 	// 按区块读取文件成List<byte[]>，(单个区块最大为10Mb)
 	public static List<byte[]> file2bList(String path, int blockNum) {
+        List<byte[]> datas = new ArrayList<>();	//返回的区块信息
 		try {
             FileInputStream inStream =new FileInputStream(new File(path));
-            List<byte[]> datas = new ArrayList<>();	//返回的区块信息
             long streamTotal = 0;	//接受流的容量
             int bytesTotal = 0;  	//流分成满1kb的byte[]的数量
             int blockTotal = 0;	//流分成满10Mb区块block的数量
             int leave = 0;  //文件剩下的字符数
             // 获得文件输入流的总量
-            streamTotal = inStream.available();
+            streamTotal = (new File(path)).length();
             // 获得流文件需要分开的满1kb byte[]的数量
-            bytesTotal = (int)Math.floor(streamTotal/MAX_BYTE);
+            bytesTotal = getBufferLength(path);
             // 获得流文件分开满10Mb区块的数量
-            blockTotal = (int)Math.floor(streamTotal/BLOCK_SIZE);
+            blockTotal = getBlockLength(path);
             // 获得分开成多个登长流文件后，最后剩余的流大小
             leave = (int)streamTotal%MAX_BYTE;
             
             // 请求区块号不匹配
-            if(blockNum < 0 || blockNum > blockTotal) return null;
+            if(blockNum < 0 || blockNum > blockTotal) return datas;
+            
+            // 区块号匹配，分批次跳过区块前面的数据
+            // jdk 9.0.1 skip源码有bug
+            for(int i = 0; i < blockNum; i++) {
+            	long skip = inStream.skip(BLOCK_SIZE);
+            	if(skip != BLOCK_SIZE) return datas;
+            }
             
             // 请求区块号是满10Mb的
-            for(int i = 0; i < blockTotal; i++) {
-            		for(int j = 0; j < BLOCK_SIZE/MAX_BYTE; j++) {
-            			byte[] data = new byte[MAX_BYTE];
-            			inStream.read(data, 0, MAX_BYTE);
-            			if(i == blockNum) datas.add(data);
-            		}
+            if(blockNum >= 0 && blockNum < blockTotal-1){
+        		for(int j = 0; j < BLOCK_SIZE/MAX_BYTE; j++) {
+        			byte[] data = new byte[MAX_BYTE];
+        			inStream.read(data, 0, MAX_BYTE);
+        			datas.add(data);
+        		}
             }
             // 请求区块号是不满10Mb的
-            if(blockNum == blockTotal) {
-            	for(int i = 0; i < bytesTotal - (BLOCK_SIZE/MAX_BYTE) * blockTotal; i++) {
+            else {
+            	for(int i = 0; i < bytesTotal - (BLOCK_SIZE/MAX_BYTE) * (blockTotal-1); i++) {
             		byte[] data = new byte[MAX_BYTE];
             		inStream.read(data, 0, MAX_BYTE);
             		datas.add(data);
             	}
+            	//剩余不满byte[MAX_BYTE]的
             	byte[] data = new byte[leave];
                 inStream.read(data, 0, leave);
                 datas.add(data);
             }
             inStream.close();
-            System.out.println("读取区块" + blockNum + "完毕! 区块大小------" + datas.size());
+            System.out.println("读取区块" + blockNum + "完毕! 区块大小------" + datas.size() + "kb.");
             return datas;
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+            return datas;
         }
 	}
 	
@@ -114,65 +123,55 @@ public class FileIO {
         }
     }
     
+    // 获得文件byte[MAX_BYTE]的数量
     public static int getBufferLength(String path) {
     	try{
-	    	FileInputStream inStream =new FileInputStream(new File(path));
-	        List<byte[]> datas = new ArrayList<>();
-	        final int MAX_BYTE = 1024;	//每个byte[]的容量,当前1Kb
-	        long streamTotal = 0;  //接受流的容量
-	        int streamNum = 0;  //流需要分开的数量
-	        int leave = 0;  //文件剩下的字符数
-	        // 获得文件输入流的总量
-	        streamTotal = inStream.available();
-	        // 获得流文件需要分开的满1kb的流的数量
-	        streamNum = (int)Math.floor(streamTotal/MAX_BYTE);
-	        inStream.close();
-	        return streamNum+1;
+    		// 获取文件 的总字节数
+    		File file = new File(path);
+    		System.out.println(file.length());
+    		long streamTotal =  file.length();
+    		return (int)Math.floor(streamTotal/MAX_BYTE);
     	} catch (Exception e) {
     		e.printStackTrace();
     		return 0;
     	}
     }
     
+    // 获得文件可以划分的区块数
     public static int getBlockLength(String path) {
     	try{
-	    	FileInputStream inStream =new FileInputStream(new File(path));
-	        List<byte[]> datas = new ArrayList<>();
-	        long streamTotal = 0;  //接受流的容量
-	        int blockNum = 0;  //流需要分开的区块数量
-	        // 获得文件输入流的总量
-	        streamTotal = inStream.available();
+    		// 获取文件 的总字节数
+    		File file = new File(path);
+    		long streamTotal =  file.length();
 	        // 获得流文件需要分开的满1kb的流的数量
-	        blockNum = (int)Math.floor(streamTotal/BLOCK_SIZE);
-	        inStream.close();
-	        return blockNum+1;
+	        return (int)Math.floor(streamTotal/BLOCK_SIZE) + 1;
     	} catch (Exception e) {
     		e.printStackTrace();
     		return 0;
     	}
     }
     
+
     
 
     public static void main(String[] args) {
-        /*List<byte[]> datas=file2byte("test.rmvb");   
-        System.out.println("readFile succeed!");
-        System.out.println("Total: " + datas.size());*/
-        System.out.println("Total: " + getBufferLength("test.mp4") + "kb.");
-        System.out.println("BlockNum: " + getBlockLength("test.mp4"));
+    	
+    	
+    	String path = "C:/Users/chenbb/Desktop/test.mp4";
+        System.out.println("Total: " + getBufferLength(path) + "kb.");
+        System.out.println("BlockNum: " + getBlockLength(path));
         String dirString = "download";
 		File file = new File(dirString);
 		if(!file.exists()) {
 			file.mkdir();
 		}
-        for(int i = 0; i < getBlockLength("test.mp4"); i++) {
-        	List<byte[]> datas = file2bList("test.mp4", i);
+		
+		for(int i = 0; i < getBlockLength(path); i++) {
+        	List<byte[]> datas = file2bList(path, i);
         	byte2file("download/test.mp4", datas);
         	System.out.println("区块" + i + "传输完毕！");
         }
-        //byte2file("output.rmvb",datas);
-        //System.out.println("saveFile succeed!");
-        //System.out.println("Total: " + getBufferLength("output.mp3") + "kb.");
+        
         
         
     }
