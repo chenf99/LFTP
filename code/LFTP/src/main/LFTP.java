fdf55d0589ebad4d9fbd8d8b27a9910739cce8f2
chenf99 <1163874966@qq.com>
2018-12-03 14:51:33 +0800

    修复导出jar包错误

--- code/LFTP/src/main/LFTP.java
+++ code/LFTP/src/main/LFTP.java
@@ -8,16 +8,15 @@ import java.util.Locale;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
-class LFTP {
+public class LFTP {
     @Parameter(names={"--clientport", "-cp"})
-    static String cport = "6000";
+    String cport = "6000";
     @Parameter(names={"--serverport", "-sp"})
-    static String sport = "7545";
+    String sport = "7545";
     @Parameter(names={"--server", "-s"})
-    static String serverAddress = "127.0.0.1";
-    @Parameter(names= {"--filename", "-f"})
-    static String fileName;
-    static Client client;
+    String serverAddress = "127.0.0.1";
+    @Parameter(names= {"--main.fileName", "-f"})
+    String fileName;
 
     public static void main(String[] argv) {
         LFTP main = new LFTP();
@@ -32,8 +31,8 @@ class LFTP {
         switch (argv[0]) {
 			case "server":
 				System.out.println("[cmd]启动服务器");
-				Server server = new Server(Integer.parseInt(sport));
-				System.out.println("[cmd]服务器消息监听端口: " + sport);
+				Server server = new Server(Integer.parseInt(main.sport));
+				System.out.println("[cmd]服务器消息监听端口: " + main.sport);
 				try {
 					server.run();
 				} catch (SocketException e) {
@@ -42,39 +41,39 @@ class LFTP {
 				break;
 			case "lget":
 				System.out.println("[cmd]从服务器接收文件");
-				System.out.println("[cmd]客户端接收端口: " + cport);
-				System.out.println("[cmd]服务器监听端口: " + sport);
-				System.out.println("[cmd]服务器地址: " + serverAddress);
-				if (fileName == null) {
+				System.out.println("[cmd]客户端接收端口: " + main.cport);
+				System.out.println("[cmd]服务器监听端口: " + main.sport);
+				System.out.println("[cmd]服务器地址: " + main.serverAddress);
+				if (main.fileName == null) {
 					System.err.println("[ERROR]文件名不能为空");
 					System.exit(0);
 				}
-				System.out.println("[cmd]文件名: " + fileName);
+				System.out.println("[cmd]文件名: " + main.fileName);
 				
-				createClient(argv[0]);
+				createClient(argv[0], main);
 				
 				break;
 			case "lsend":
 				System.out.println("[cmd]向服务器发送文件");
-				System.out.println("[cmd]客户端发送端口: " + cport);
-				System.out.println("[cmd]服务器监听端口: " + sport);
-				System.out.println("[cmd]服务器地址: " + serverAddress);
-				if (fileName == null) {
+				System.out.println("[cmd]客户端发送端口: " + main.cport);
+				System.out.println("[cmd]服务器监听端口: " + main.sport);
+				System.out.println("[cmd]服务器地址: " + main.serverAddress);
+				if (main.fileName == null) {
 					System.err.println("[ERROR]文件名不能为空");
 					System.exit(0);
 				}
-				System.out.println("[cmd]文件名: " + fileName);
+				System.out.println("[cmd]文件名: " + main.fileName);
 				
-				createClient(argv[0]);
+				createClient(argv[0], main);
 				
 				break;
 			case "listall":
 				System.out.println("[cmd]列出服务器目录下所有文件");				
-				System.out.println("[cmd]客户端端口: " + cport);				
-				System.out.println("[cmd]服务器端口: " + sport);				
-				System.out.println("[cmd]服务器地址: " + serverAddress);
+				System.out.println("[cmd]客户端端口: " + main.cport);				
+				System.out.println("[cmd]服务器端口: " + main.sport);				
+				System.out.println("[cmd]服务器地址: " + main.serverAddress);
 				
-				createClient(argv[0]);
+				createClient(argv[0], main);
 				
 				break;
 			default:
@@ -83,15 +82,15 @@ class LFTP {
 		}
     }
     
-    private static void createClient(String operation) {
+    private static void createClient(String operation, LFTP main) {
     	InetAddress address = null;
 		try {
-			address = InetAddress.getByName(serverAddress);
+			address = InetAddress.getByName(main.serverAddress);
 		} catch (UnknownHostException e) {
 			System.err.println("解析服务器地址出错");
 		}
 		if (address == null) System.exit(0);
-		client = new Client(Integer.parseInt(cport), operation, fileName, address, Integer.parseInt(sport));
+		Client client = new Client(Integer.parseInt(main.cport), operation, main.fileName, address, Integer.parseInt(main.sport));
 		client.run();
     }
 }
